3563fb23e8473263a6d46554f15dff748fd7ad95
little-ai <fakelittleaqqff@protonmail.com>
2023-11-19 13:33:58 +0300

    The foundations of read were laid

--- src/app.py
+++ src/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+def is_root_directory(path):
+    return path == "/"
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    base_directory = os.path.expanduser("~")
+    files_and_folders = os.listdir(base_directory)
+
+    selected_item = None
+    selected_item_content = None
+
+    if request.method == 'POST':
+        selected_item = request.form.get('selected_item')
+
+        if selected_item:
+            selected_item_path = os.path.join(base_directory, selected_item)
+
+            if os.path.isdir(selected_item_path):
+                selected_item_content = os.listdir(selected_item_path)
+
+    return render_template('index.html', files_and_folders=files_and_folders, selected_item=selected_item, selected_item_content=selected_item_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
